5e32cce519e2483c794171381320f042cd1edc23
Abhinav Prajapati <abhinavprajapati2001@gmail.com>
2025-05-04 02:56:35 +0530

    add "use client" directive and import React in BlogComponents

--- components/blogComponets.tsx
+++ components/blogComponets.tsx
@@ -1,3 +1,4 @@
+"use client";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ReactNode, useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
